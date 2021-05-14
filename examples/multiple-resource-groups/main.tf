f18c6397f2781bdc0051679599ca9cd9acfb811e
cybergavin <cybergavin@gmail.com>
2021-05-13 23:34:07 -0400

    Modified examples directory structure - v1.1.0

--- examples/multiple-resource-groups/main.tf
+++ examples/multiple-resource-groups/main.tf
@@ -1,6 +1,6 @@
 module "resource-group" {
-  source  = "cybergavin/rg/azurerm"
-  version = "1.0.0"
+  source  = "cybergavin/resource-group/azurerm"
+  version = "1.1.0"
   resource_groups = var.resource_groups
   global_tags     = var.global_tags
 }
