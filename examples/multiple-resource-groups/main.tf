cf60c7d332ecacc5fda105e9d0fd5b9da5d2d189
cybergavin <cybergavin@gmail.com>
2021-05-13 23:36:54 -0400

    v1.2.0

--- examples/multiple-resource-groups/main.tf
+++ examples/multiple-resource-groups/main.tf
@@ -1,6 +1,6 @@
 module "resource-group" {
   source  = "cybergavin/resource-group/azurerm"
-  version = "1.1.0"
+  version = "1.2.0"
   resource_groups = var.resource_groups
   global_tags     = var.global_tags
 }
